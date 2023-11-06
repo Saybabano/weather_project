2811f97049b456789cf89434b683154555c366fa
saybabano <sayba22@navgurukul.org>
2023-11-06 20:49:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Weather from './Components/WeatherApp/Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+    </div>
+  );
+}
+
+export default App;
